8a58f667eac7fa7444bd4c5f0b1dc3ba39ff0513
EBuravskiy <elric0sphinx@gmail.com>
2025-07-22 16:47:05 +0500

    Create UI View

---
+++ AppTaskManager/AppTaskManager/Views/TaskManagerMainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppTaskManager.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для TaskManagerMainWindow.xaml
+    /// </summary>
+    public partial class TaskManagerMainWindow : Window
+    {
+        public TaskManagerMainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
