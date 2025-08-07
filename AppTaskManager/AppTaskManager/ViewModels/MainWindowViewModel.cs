f8a5def8a42105e0c0eb457665e096dbe5bac93e
EBuravskiy <elric0sphinx@gmail.com>
2025-08-07 09:10:58 +0500

    Added display of the selected task

--- AppTaskManager/AppTaskManager/ViewModels/MainWindowViewModel.cs
+++ AppTaskManager/AppTaskManager/ViewModels/MainWindowViewModel.cs
@@ -2,21 +2,62 @@
 using AppTaskManager.Models;
 using AppTaskManager.Views;
 using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace AppTaskManager.ViewModels
 {
     public class MainWindowViewModel : BaseObservableClass
     {
-       
+
+        private TaskModel _selectedTask;
+        public TaskModel SelectedTask
+        {
+            get { return _selectedTask; }
+            set 
+            { 
+                _selectedTask = value;
+
+                OnPropertyChanged(nameof(SelectedTask));
+            }
+        }
+
         public ITaskController TaskController;
 
         public ObservableCollection<TaskModel> TaskModels { get; private set; }
+
+        private string _searchTitle;
+        public string SearchTitle
+        {
+            get { return _searchTitle; }
+            set 
+            { 
+                _searchTitle = value; 
+                OnPropertyChanged(nameof(SearchTitle));
+            }
+        }
+
         public MainWindowViewModel()
         {
             TaskController = new MockTaskController();
             TaskModels = new ObservableCollection<TaskModel>();
             LoadTasksFromController();
+            SelectedTask = new TaskModel()
+            {
+                Id = 0,
+                Title = "Наименование задачи",
+                Description = "Описание задачи",
+                CreationTime = DateTime.Now,
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now,
+                IsCompleted = false,
+                TaskState = TaskState.Create,
+                TaskCategory = TaskCategory.Work,
+                TaskImportance = TaskImportance.Low,
+                TaskChecks = new List<TaskCheck>()
+            };
         }
 
         private void LoadTasksFromController()
@@ -32,6 +73,37 @@ namespace AppTaskManager.ViewModels
         {
             TaskModels.Add(newTask);
             OnPropertyChanged(nameof(TaskModel));
+        }
+
+        public ICommand IFindTasks => new RelayCommand(find => FindTitleInTasks());
+        public void FindTitleInTasks()
+        {
+            ObservableCollection<TaskModel> searchTask = new ObservableCollection<TaskModel>();
+            if (SearchTitle == String.Empty)
+            {
+                LoadTasksFromController();
+                return;
+            }
+            foreach (TaskModel task in TaskModels)
+            {
+                var title = task.Title.ToLower();
+                if (title.Contains(_searchTitle.ToLower()))
+                {
+                    searchTask.Add(task);
+                }
+            }
+            if (searchTask.Count != 0)
+            {
+                TaskModels.Clear();
+                foreach (TaskModel task in searchTask)
+                {
+                    TaskModels.Add(task);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Задача с таким названием отсутствует");
+            }
         }
 
         public ICommand IOpenNewWindow => new RelayCommand(open => OpenNewWindow());
